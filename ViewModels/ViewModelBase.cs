e06722133a72bf775431f8df4df35f2467681af7
Marcin Szablak <m.szablak30@gmail.com>
2024-09-30 20:28:50 +0200

    First Commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NoNameMusicApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NoNameMusicApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
