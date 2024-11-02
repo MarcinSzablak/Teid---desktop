0492ccdc894aebce6ec83fd66889d2140c02292c
IB2R5I <danielgarbuszewski2@gmail.com>
2024-11-02 22:11:24 +0100

    setup basic layout

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 pygame.mixer.init()
 sound = pygame.mixer.Sound('Rogue-Dreams.mp3')
+sound.set_volume(0.2)
 sound.play()
 
 root = tk.Tk()
@@ -11,16 +12,37 @@ root.title("My Tkinter App")
 root.geometry("1280x720")
 root.minsize(960, 506)
 
-label3 = tk.Label(root, text="Hello, World!", background="white")
-label3.pack(side="top",fill="both")
+frame_top = tk.Frame(root, background="white")
+frame_top.pack(side="top", fill="x")
 
-label = tk.Label(root, text="Hello, World!", background="red")
-label.pack(side="bottom",fill="both")
+frame_bottom = tk.Frame(root, background="red")
+frame_bottom.pack(side="bottom", fill="x")
 
-label2 = tk.Label(root, text="Hello, World!", background="yellow")
-label2.pack(side="left",expand=True,fill="both")
+frame_left = tk.Frame(root, background="yellow")
+frame_left.pack(side="left", fill="y")
 
-label4 = tk.Label(root, text="Hello, World!", background="blue")
-label4.pack(side="left",expand=True,fill="both")
+top_label = tk.Label(frame_left, text="lala", bg="brown")
+top_label.pack(pady=10)
+
+frame_right = tk.Frame(root, background="blue")
+frame_right.pack(side="left", fill="y")
+
+def set_frame_heights():
+    total_height = root.winfo_height()
+    total_width = root.winfo_width()
+    top_height = int(total_height * 0.05)
+    bottom_height = int(total_height * 0.10)
+    left_width = int(total_width * 0.8)
+    right_width = int(total_width * 0.21)
+
+    frame_top.config(height=top_height)
+    frame_bottom.config(height=bottom_height)
+    frame_left.config(width=left_width)
+    frame_right.config(width=right_width)
+
+
+root.bind("<Configure>", lambda e: set_frame_heights())
+
+set_frame_heights()
 
 root.mainloop()
